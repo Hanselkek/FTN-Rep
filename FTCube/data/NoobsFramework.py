9f57631ed0ae9ee3ae91c167f79eedb2c10c2804
Hanselkek <87748222+Hanselkek@users.noreply.github.com>
2022-03-01 09:44:59 +0800

    Add files via upload

--- FTCube/data/NoobsFramework.py
+++ FTCube/data/NoobsFramework.py
@@ -18,12 +18,22 @@ class update:
 class show:
     def show_ui(self):
         self.text = pygame.font.SysFont('system', 28)
-        self.render = self.text.render("Found Noobs: " + str(FoundNoobs), True, (255, 255, 255))
+        self.render = self.text.render("Found Noobs: " + str(FoundNoobs), True, (165, 255, 255))
 
         return self.render
 class showRec:
     def showRecent(self):
         self.recent = pygame.font.SysFont('system', 22)
-        self.render = self.recent.render("Recent Noob: " + RecentNoob, True, (255, 255, 255))
+        self.render = self.recent.render("Recent Noob: " + RecentNoob, True, (165, 255, 255))
         
         return self.render
+class showKeys:
+    def option(self, x, y, obj, screen):
+
+        screen.blit(obj, (x, y))
+
+    def create(self, wantedString, fontSize):
+            self.wanted = pygame.font.SysFont('system', fontSize)
+            self.renderedWanted = self.wanted.render(wantedString, True, (165, 255, 255))
+
+            return self.renderedWanted
